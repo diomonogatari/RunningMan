91de6b4ffb55552bfce8e8785734855654343735
Diogo Rosa Carvalho <diodude.lol@gmail.com>
2020-08-18 17:26:18 +0100

    Rotating coins & prefab

---
+++ Assets/Scripts/RotateCoin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCoin : MonoBehaviour
+{
+    public int rotateSpeed = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+}
